4973829a0fe3dea7a777224d885d3f6b936c8272
Johan Marlo Cabili <jmtcabili@gmail.com>
2024-07-09 18:21:23 +0800

    m

--- main.c
+++ main.c
@@ -2,33 +2,48 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-extern void flipVerticall(uint8_t img2[3][3][3],
-                          uint8_t img1[3][3][3],
+extern void flipVerticall(uint8_t img2[5][4][3],
+                          uint8_t img1[5][4][3],
                           int m,
                           int h);
 
 int main(){
 
-    int m = 3, h = 3; 
-    uint8_t inputArray[3][3][3] = {
+    int m = 5, h = 4; 
+    uint8_t inputArray[5][4][3] = {
                                 {
                                     {1, 2, 3},
                                     {4, 5, 6},
-                                    {7, 8, 9}
+                                    {7, 8, 9}, 
+                                    {10, 2, 3}
                                 }, 
                                 {
                                     {1, 2, 6},
                                     {2, 6, 7},
-                                    {9, 4, 8}
+                                    {9, 4, 8}, 
+                                    {1, 2, 2}
                                 }, 
                                 {
                                     {1, 5, 7},
                                     {1, 3, 3},
-                                    {3, 4, 6}
+                                    {3, 4, 6}, 
+                                    {0, 4, 2}
+                                }, 
+                                {
+                                    {12, 22, 60},
+                                    {224, 6, 47},
+                                    {12, 4, 8}, 
+                                    {4, 2, 1}
+                                },
+                                {
+                                    {9, 4, 8}, 
+                                    {1, 2, 2},
+                                    {224, 6, 47},
+                                    {2, 6, 7}
                                 }
     };
     
-    uint8_t outputArray[3][3][3] = {0}; 
+    uint8_t outputArray[5][4][3] = {0}; 
 
     flipVerticall(outputArray, inputArray, m, h);
 
